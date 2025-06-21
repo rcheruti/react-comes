eafeb3cf01d1d81c1458e6b2053f8aa3ecb0755e
RCC <rcc@gmail.com>
2025-06-21 00:59:43 -0300

    Docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '',
   server: {
     host: true, // irá ouvir em todas as portas, inclusive o IP 'público'
     proxy: { // exemplo de proxy
